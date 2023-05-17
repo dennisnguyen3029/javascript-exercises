b68646c69fc719815882af8d2136eea080a9bc33
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-05-17 15:39:05 -0400

    Complete sumAll module

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
 const sumAll = function() {
+    let res = 0;
+    let lower = Math.min(arguments[0], arguments[1]);
+    let upper = Math.max(arguments[0], arguments[1]);
+    if (
+        arguments.length !== 2 ||
+        isNaN(arguments[0]) || isNaN(arguments[1]) ||
+        typeof(arguments[0]) === 'string' || typeof(arguments[1]) === 'string' ||
+        lower < 0 || upper < 0
+    ) { return "ERROR";}
+    for (let i = lower; i <= upper; i++) {
+        res += i;
+    }
 
+    return res;
 };
 
+console.log(sumAll(1, '90'));
 // Do not edit below this line
 module.exports = sumAll;
