8ce8c12a823e7bf3b181ca6ea9c41d252e092af2
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-05-17 16:06:14 -0400

    Complete leapYear module

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
 const leapYears = function() {
+    year = arguments[0];
+    if (year % 4 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else if (year % 100 === 0) {
+            return false;
+        } else return true;
+    } else return false; 
 
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(20));
+console.log(leapYears(1985));
+console.log(leapYears(1800));
+console.log(leapYears(40));
 
 // Do not edit below this line
 module.exports = leapYears;
