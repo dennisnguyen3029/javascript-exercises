162e19b688f416e6ccc50561ee6e7f3a2156b0cd
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-05-17 12:14:08 -0400

    Complete reverseString module

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // let i = str.length - 1;
+    // let result = "";
+    // while (i >= 0) {
+    //     result += str[i];
+    //     i--;
+    // }
 
+    // return result;
+
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
