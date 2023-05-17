218055391f6ddc7c22da83a28bdeae457fcb0fca
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-05-17 13:21:01 -0400

    Complete removeFromArray module

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
 const removeFromArray = function() {
+    allArgs = Array.from(arguments);
+    originalArray = allArgs[0];
+    replacedValues = allArgs.slice(1);
+    for (const val of replacedValues) {
+        const index = originalArray.indexOf(val);
+        if (index < 0) continue; 
+        originalArray.splice(index, 1);
 
+    }
+
+    return originalArray;
+    
 };
+
+testArr = [1,2,3,4];
+console.log(testArr.indexOf(5));
+console.log(testArr.splice(-1,1));
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
