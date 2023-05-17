bf571d70fc85f27dd5b9ed567914cec689c8dace
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-05-17 16:20:00 -0400

    Complete tempConversion module

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return Math.round((degreesF - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  return Math.round((degreesC * (9/5) + 32) * 10) / 10;
 };
 
+
+console.log(convertToCelsius(42.21));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
