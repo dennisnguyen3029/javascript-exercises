bc3b9038af599b402b105f36cc66c6ae023d812c
dennisnguyen3029 <dennis.nguyen3029@gmail.com>
2023-06-19 15:19:39 -0400

    Added functions for calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // 0 is the initial
+  // sum is our accumulator
+  // current is our current item in the array
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  }
+  res = 1;
+  for (let i = 1; i <= num; i++) {
+    res *= i
+  }
+
+  return res;
+
+  
 };
 
 // Do not edit below this line
